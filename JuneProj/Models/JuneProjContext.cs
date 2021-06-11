83dd40a0d1fe175a1d2b50010b1b37679d7f7a1d
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-11 16:48:58 -0700

    begin context model

---
+++ JuneProj/Models/JuneProjContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace JuneProj.Models
+{
+  public class JuneProjContext : DbContext
+  {
+
+  }
+}
